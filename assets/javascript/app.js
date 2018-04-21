8e036c029a72d10a125c72a517eb62816480290d
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-04-21 08:00:59 -0500

    Expanded on the data attributes of the buttons and reworking the code to pull GIFs based on button clicks rather than the submit click. Working on code to change gifs from still to animated urls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,15 +21,18 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var topics = [];
+var topics = ["the office","parks and recreation","brooklyn 99","psych"];
 $(document).ready(function() {
   $("#clear-GIFs").on("click", function(event) {
     //clear gifs on page and buttons created
+
   });
-  $("#get-GIFs").on("click", function(event) {
-    event.preventDefault();
+
+  $(".get-GIFs").on("click", function() {
+
+    var search = $(this).data("search");
+    console.log(search);
     var apiKey = "8ZCbydXTYkPNLN7AffE0dj7uiERD5sZZ";
-    var search = $(".search-GIF").val();
     var limit = $(".gif-amount").val();
     var queryURL =
       "https://api.giphy.com/v1/gifs/search?q=" +
@@ -43,20 +46,34 @@ $(document).ready(function() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        console.log(queryURL);
+      console.log(queryURL);
       var gifList = response.data;
 
       for (i = 0; i < limit; i++) {
-        var rated = gifList[i].rating;
-        var gif = gifList[i].images.fixed_height_still.url;
-        $(".main-feed").append(
+        var rated = "rating:" + gifList[i].rating;
+        var gifStill = gifList[i].images.fixed_height_still.url;
+        var gifAnimated = gifList[i].images.fixed_height.url;
+
+        $(".main-feed").prepend(
           `<div class=GIFs>
-          <h2>${rated}</h2>
-          <img src=${gif}>
+          <h4>${rated}</h2>
+          <img src=${gifStill} data-still=${gifStill} data-animated=${gifAnimated} data-state="still">
+          
           </div>`
         );
+var $img = $(this);
+        var state = $(this).attr("data-state");
+        
+        if (state == "still") {
+        //   var animatedImg = $img.attr("data-animate");
+
+          $img.attr({ src: gifAnimated, "data-state": "animate" });
+        } else {
+        //   var stillImg = $img.attr("data-still");
+
+          $img.attr({ src: gifStill, "data-state": "still" });
+        }
       }
-    
     });
   });
 });
